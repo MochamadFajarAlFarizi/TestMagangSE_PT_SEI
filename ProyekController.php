2dc983bcd1eee71826f70c7b11f315f9b9a31049
MochamadFajarAlFarizi <127916073+MochamadFajarAlFarizi@users.noreply.github.com>
2024-08-18 02:53:54 +0700

    first commit

--- ProyekController.php
+++
@@ -1,8 +0,0 @@
-class ProyekController extends CI_Controller {
-    public function index() {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'http://localhost:8081/proyek');
-        $data['proyek'] = json_decode($response->getBody());
-        $this->load->view('proyek/index', $data);
-    }
-}
